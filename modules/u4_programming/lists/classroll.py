9d883f9afd8a965534d9013db67d40a0d07055ec
Alexander Herbert <her0014@northcote-hs.edu.vic.gov.au>
2018-05-03 10:36:57 +1000

    finished class roll

--- modules/u4_programming/lists/classroll.py
+++ modules/u4_programming/lists/classroll.py
@@ -1,3 +1,6 @@
 roll = ['Jessica', 'Emily', 'Jordan', 'Kayley', 'Bruce', 'Michael', 'Everett', 'Lisa', 'Sam', 'Noah']
 print(roll[2])
 enrolment=len(roll)
+roll.append('James')
+del roll[2]
+roll[4] = 'Mike'
