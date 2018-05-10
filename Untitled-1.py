8eae7ba2022a1abda53fbb8b122401c917c08118
alex herbert <37639670+bigmcthankiesfrommcspankies@users.noreply.github.com>
2018-05-10 18:15:31 +1000

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,12 +2,12 @@
 File Name: TriviaQuiz.py
 Date: 05-08-18
 Author: Alex Herbert
-Description: A program that tests users knowledge on a variety of topics.
+Description: A program that tests users knowledge on streetwear related questions.
 """
 
 countdown = ['3','2','1','Next Question!','']
-questionlist = ["Question 1: What colour is Supreme's classic bogo? ",'Question 2: What country did Bape originate in? ', 'How many collaborations have The North Face and Supreme done?']
-answerlist = ['red', 'japan', 'two']
+questionlist = ["Question 1: What colour is Supreme's classic bogo? ",'Question 2: What country did Bape originate in? ', 'How many collaborations have The North Face and Supreme done? ','How many shoe collaborations have Off-White and Nike done? ']
+answerlist = ['red', 'japan', 'two', 'ten']
 def ask():
     answer = input(q)
     finalanswer = answer.lower()
@@ -17,6 +17,7 @@ def ask():
             print(item)
     if finalanswer != a:
         print('Incorrect :(')
+        print('The correct answer was' + answer)
         for item in countdown:
             print(item)
 
@@ -31,4 +32,8 @@ ask()
 
 q = questionlist[2]
 a = answerlist[2] 
+ask()
+
+q = questionlist[3]
+a = answerlist[3]
 ask()
