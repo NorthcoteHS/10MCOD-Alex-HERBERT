f6a745216aecd23aabd4b4d8078a3f09aaccf3f2
alex herbert <37639670+bigmcthankiesfrommcspankies@users.noreply.github.com>
2018-05-10 10:33:59 +1000

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,37 @@
+"""
+File Name: TriviaQuiz.py
+Date: 05-08-18
+Author: Alex Herbert
+Description: A program that tests users knowledge on a variety of topics.
+"""
+score = int(0)
+countdown = ['3','2','1','Next Question!','']
+questionlist = ["Question 1: What colour is Supreme's classic bogo? ",'Question 2: What country did Bape originate in? ', 'How many collaborations have The North Face and Supreme done?']
+answerlist = ['red', 'japan', 'two']
+def ask():
+    answer = input(q)
+    finalanswer = answer.lower()
+    if finalanswer == a:
+        print('Correct!')
+        score = score + 1
+        print("Your score is" + score)
+        for item in countdown:
+            print(item)
+    if finalanswer != a:
+        print('Incorrect :(')
+        print("Your score is" + score)
+        for item in countdown:
+            print(item)
+
+
+q = questionlist[0]
+a = answerlist[0]
+ask()
+
+q = questionlist[1]
+a = answerlist[1]
+ask()
+
+q = questionlist[2]
+a = answerlist[2] 
+ask()
